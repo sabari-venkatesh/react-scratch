3cdaeb636c1531aa719ee1cbb3630dba5248a916
sabari <sabarivenkatesh.raja@objectfrontier.com>
2020-03-18 12:42:19 +0530

    removed comments

--- config/webpack.parts.js
+++ config/webpack.parts.js
@@ -59,43 +59,4 @@ const getPlugins = () => {
   ].filter(Boolean)
 }
 
-
-// const processCSS = () => ({
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: [
-//           __ISDEV__ ? "style-loader" : MiniCssExtractPlugin.loader,
-//           {
-//             loader: "css-loader",
-//             options: {
-//               modules: {
-//                 localIdentName: "[name]_[local]_[hash:base64:3]"
-//               },
-//               importLoaders: 1
-//             }
-//           },
-//           {
-//             loader: "postcss-loader",
-//             options: {
-//               ident: "postcss",
-//               config: {
-//                 path: path.resolve(__dirname, "postcss.config.js")
-//               }
-//             }
-//           }
-//         ]
-//       }
-//     ]
-//   },
-//   plugins: [
-//     // Output extracted CSS to a file
-//     new MiniCssExtractPlugin({
-//       filename: __ISDEV__ ? "[name].css" : "[name].[chunkhash:8].css",
-//       chunkFilename: __ISDEV__ ? "[id].css" : "[id].[chunkhash:8].css"
-//     }),
-//   ]
-// });
-
 export { getStyleLoaders, getPlugins };
