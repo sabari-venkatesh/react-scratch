e77ef1948e976be635e6a49fec968bd84fe62a2b
sabari <sabarivenkatesh.raja@objectfrontier.com>
2020-03-20 23:39:10 +0530

    babel config separated

---
+++ config/babel.config.js
@@ -0,0 +1,26 @@
+const pkg = require("../package.json");
+
+module.exports = (api) => {
+  // https://babeljs.io/docs/en/config-files#apicache
+  // api.env("development") returns true if development environment
+  api.cache.using(() => api.env("development"));
+  return {
+    presets: [
+      // A Babel preset that can automatically determine the Babel plugins and polyfills
+      // https://github.com/babel/babel-preset-env
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            browsers: pkg.browserslist[api.env()],
+          },
+          useBuiltIns: false,
+          modules: false, // Needed for tree shaking to work.
+          debug: true,
+        },
+      ],
+      ["@babel/preset-react", { development: api.env("development") }]
+    ],
+    plugins: []
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+// regex patterns 
+
+
+
 const config = (env, argv) => {
   const mode = argv.mode || "development";
   const __ISDEV__ = mode !== "production";
 
+  const IMAGES = /\.(bmp|gif|jpg|jpeg|png|svg)$/;
+  const STYLES = /\.css$/i;
+  const ASSET_LIMIT = 4096;
+  const ASSET_NAME = __ISDEV__
+    ? "[path][name].[ext]?[hash:8]"
+    : "[hash:8].[ext]";
   return {
     mode,
     devtool: __ISDEV__ ? "cheap-module-source-map" : "source-map",
@@ -23,95 +33,116 @@ const config = (env, argv) => {
       devServer: {
         historyApiFallback: true,
         compress: true,
-        clientLogLevel: 'none',
+        clientLogLevel: "none",
         hot: true,
         overlay: false,
         open: true,
       }
     }),
     module: {
-      rules: [{
-        test: /\.css$/i,
-        exclude: /node_modules/,
-        use: [__ISDEV__ ? "style-loader" : MiniCssExtractPlugin.loader,
+      rules: [
+        // rules for styles
         {
-          loader: "css-loader",
-          options: {
-            importLoaders: 1,
-            sourceMap: !__ISDEV__,
-            modules: {
-              localIdentName: '[name]__[local]___[hash:base64:3]',
-            }
+          test: /\.css$/i,
+          exclude: /node_modules/,
+          use: [__ISDEV__ ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              sourceMap: !__ISDEV__,
+              modules: {
+                localIdentName: __ISDEV__
+                  ? "[name]-[local]-[hash:base64:5]"
+                  : "[hash:base64:5]",
+              }
+            },
           },
-        },
-        {
-          loader: "postcss-loader",
-          options: {
-            ident: "postcss",
-            plugins: [
-              require("postcss-import")(),
-              require('postcss-calc')(),
-              require('postcss-flexbugs-fixes')(),
-              require("postcss-preset-env")({
-                stage: 1,
-                autoprefixer: {
-                  flexbox: 'no-2009',
-                },
-                // https://github.com/csstools/postcss-preset-env/blob/master/src/lib/plugins-by-id.js#L36
-                features: {
-                  "nesting-rules": true,
-                  "color-functional-notation": true
-                },
-                browsers: pkg.browserslist[process.env.NODE_ENV]
-              }),
-              ...(__ISDEV__ ? [] : [
-                require("cssnano")({
-                  preset: ["default", {
-                    discardComments: { removeAll: true }
-                  }]
-                })
-              ])
-            ],
-            sourceMap: !__ISDEV__
-          }
-        }],
-      }, {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: "url-loader",
-        options: {
-          limit: 10000,
-          name: 'img/[name].[hash:8].[ext]',
-        },
-      }, {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: "babel-loader",
-          options: {
-            babelrc: false,
-            configFile: false,
-            presets: [
-              // A Babel preset that can automatically determine the Babel plugins and polyfills
-              // https://github.com/babel/babel-preset-env
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    browsers: pkg.browserslist[mode],
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [
+                require("postcss-import")(),
+                require("postcss-calc")(),
+                require("postcss-flexbugs-fixes")(),
+                require("postcss-preset-env")({
+                  stage: 1,
+                  autoprefixer: {
+                    flexbox: "no-2009",
                   },
-                  useBuiltIns: "entry",
-                  forceAllTransforms: !__ISDEV__, // for UglifyJS
-                  modules: false,
-                  useBuiltIns: false,
-                  debug: true,
-                },
+                  // https://github.com/csstools/postcss-preset-env/blob/master/src/lib/plugins-by-id.js#L36
+                  features: {
+                    "nesting-rules": true,
+                    "color-functional-notation": true
+                  },
+                  browsers: pkg.browserslist[mode]
+                }),
+                ...(__ISDEV__ ? [] : [
+                  require("cssnano")({
+                    preset: ["default", {
+                      discardComments: { removeAll: true }
+                    }]
+                  })
+                ])
               ],
-            ],
-            // cacheDirectory: false,
-            // compact: !__ISDEV__
-          }
-        }]
-      }
+              sourceMap: !__ISDEV__
+            }
+          }],
+        },
+
+        // rules for images
+        {
+          test: IMAGES,
+          oneOf: [
+            {
+              issuer: STYLES,
+              oneOf: [
+                {
+                  test: /\.svg$/,
+                  loader: "svg-url-loader",
+                  options: {
+                    name: ASSET_NAME,
+                    limit: ASSET_LIMIT, // 4kb
+                  },
+                },
+                {
+                  loader: "url-loader",
+                  options: {
+                    name: ASSET_NAME,
+                    limit: ASSET_LIMIT, // 4kb
+                  },
+                }
+              ]
+            },
+
+            {
+              loader: "file-loader",
+              options: {
+                name: ASSET_NAME,
+              },
+            }
+          ]
+        },
+
+        // rules for scripts
+        {
+          test: /\.(js|jsx)$/,
+          include: resolve(__dirname, "src"),
+          exclude: /node_modules/,
+          use: [{
+            loader: "babel-loader",
+            options: {
+              root: resolve(__dirname, "config"),
+              babelrc: false,
+              envName: mode,
+              cacheDirectory: __ISDEV__,
+              cacheCompression: false,
+              compact: !__ISDEV__,
+              ignore: ["node_modules", "build"],
+            }
+          }]
+        }
       ]
     },
     plugins: [
